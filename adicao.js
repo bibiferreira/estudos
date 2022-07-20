57f3ae35f7d64b6a4c5a1fa93c245aaf349eed48
Bibi Ferreira <bibi.espeiorin@gmail.com>
2022-07-20 09:43:40 +0200

    adicionado algoritmo que soma dois números e é adicionado na tela caso seja maior que 10.

---
+++ adicao.js
@@ -0,0 +1,12 @@
+//  Construa um algoritmo que leia dois valores numéricos inteiros e efetue a adição; caso o resultado seja maior que 10, imprima-o na tela.
+
+const prompt = require('prompt-sync')();
+
+const num1 = Number(prompt('Digite o primeiro número: '));
+const num2 = Number(prompt('Digite o segundo número: '));
+
+const soma = num1 + num2;
+
+  if (soma > 10) {
+    return console.log('A soma dos dois números é maior que 10');
+  }
