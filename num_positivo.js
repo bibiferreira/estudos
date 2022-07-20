26494d803a0751a46ea6ee4a500ea3b5bd36154d
Bibi Ferreira <bibi.espeiorin@gmail.com>
2022-07-20 09:52:43 +0200

    adicionado algoritmo que diz se o número é positivo, negativo ou neutro.

---
+++ num_positivo.js
@@ -0,0 +1,13 @@
+// Escreva um algoritmo para determinar se um dado número N (digitado pelousuário) é positivo, negativo ou neutro (zero). Imprima o resultado na tela.
+
+const prompt = require('prompt-sync')();
+
+const numero = Number(prompt('Digite um número: '));
+
+if (numero > 0) {
+  return console.log(`O número ${numero} é positivo`);
+} else if (numero < 0) {
+  return console.log(`O número ${numero} é negativo`);
+} else {
+  return console.log(`${numero} é neutro`);
+}
