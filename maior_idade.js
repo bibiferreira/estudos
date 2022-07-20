bfd19fdf02a52497aad072a8a94da2be8fffe6e7
Bibi Ferreira <bibi.espeiorin@gmail.com>
2022-07-20 10:20:05 +0200

    adicionado algoritmo que diz se a pessoa é maior de idade, menor de idade ou idosa.

---
+++ maior_idade.js
@@ -0,0 +1,13 @@
+// Escreva um algoritmo que receba a idade de uma pessoa e indique se ela é: menor de idade, maior de idade, idosa (60 anos ou mais).
+
+const prompt = require('prompt-sync')();
+
+const idade = Number(prompt('Digite a sua idade: '));
+
+if (idade >= 18 && idade < 60) {
+  return console.log('Você é maior de idade!');
+} else if (idade < 18) {
+  return console.log('Você é menor de idade!');
+} else {
+  return console.log('Você é idoso(a)!');
+}
