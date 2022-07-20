4218ff99c2797a18f7716374adc3e4d059446bed
Bibi Ferreira <bibi.espeiorin@gmail.com>
2022-07-20 09:25:04 +0200

    adicionado algoritmo que leia numero maior que 20

---
+++ numero_maior.js
@@ -0,0 +1,9 @@
+// Escreva um algoritmo que leia um número e o imprima na tela caso seja maior que 20.
+
+const prompt = require('prompt-sync')();
+
+const numero = Number(prompt('Digite um número: '));
+
+if(numero > 20) {
+  return console.log(`O número ${numero} é maior que 20.`);
+}
